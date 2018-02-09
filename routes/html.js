10a72466690a721f66ee0eb6211f1a84503d76d9
Dale Holstein <DaleHolstein@wifi_stu-10-38-65-99.uncc.edu>
2018-02-08 20:15:31 -0500

    routes initial

--- routes/html.js
+++ routes/html.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const api = require("./api");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+// GET home page
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// GET Detailers page (FILL IN CORRECT INFO IF NECESSARY)
+router.get('/detail', function (req, res, next) {
+  res.render('detail', { title: 'Express' });
+});
+
+// GET Booking Page
+router.get('/booking', function (req, res, next) {
+  res.render('booking', { title: 'Express' });
+});
+
+// API routes
+router.use("/api", api);
 
 module.exports = router;
