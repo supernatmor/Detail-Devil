f7cb69a69e786ae3e6e480041057094a4fad2e26
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2018-02-09 15:18:54 -0500

    html routes for index working

--- routes/html.js
+++ routes/html.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const api = require("./api");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// GET home page
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Home' });
 });
+
+// GET Detailers page (FILL IN CORRECT INFO IF NECESSARY)
+router.get('/detail', function (req, res, next) {
+  res.render('detail', { title: 'Detail' });
+});
+
+// GET Booking Page
+router.get('/booking', function (req, res, next) {
+  res.render('booking', { title: 'Booking' });
+});
+
+// API routes
+// router.use("/api", api);
 
 module.exports = router;
