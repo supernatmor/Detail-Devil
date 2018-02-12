e256651245458d8493bc271d5008f131bdffb27e
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2018-02-12 00:35:09 -0500

    initial routes for user login

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,19 @@
 const router = require("express").Router();
 const companyController = require("../controllers/companyController");
-// const userController = require("../controllers/userController.js");
+const userController = require("../controllers/userController.js");
 
 // Routes
 
 // Route to get info from company collection
 router.route("/detail").get(companyController.findAll);
 
-// // Booking. Get customer info if logged in
-// // router.route("user/:id").get(userController.currentUser);
+// Create User
+router.route("/user/create").post(userController.createUser);
 
-// // User Sign UP. POST Info
-// // router.route("/user/login").post(userController.newUser);
+// User Login
+router.route("user/:id").get(userController.userLogin);
 
+// User Profile
+// router.route("/profile").get(userController.userProfile);
 
 module.exports = router;
