e73598fddfb14a5a38f2c0e5b20320d59848bf22
Dale Holstein <DaleHolstein@wifi_stu-10-38-65-204.uncc.edu>
2018-02-22 22:53:47 -0500

    fixed

--- routes/api.js
+++ routes/api.js
@@ -7,69 +7,64 @@ const bcrypt = require("bcrypt");
 const expressSanitizer = require("express-sanitizer");
 
 // API Routes
-
+let key = "5a861f0825b7151665143fee";
 // Route to get info from company collection
 router.route("/detail").get(companyController.findAll);
 
 // Post booking info
-router.put("/booking/", function (req, res) {
-    if (session) {
-        userController.createBooking(req.params.id, req.body)
-    } else {
-        console.log("no session found");
-    }
+router.put("/booking/", function(req, res) {
+  // if (session) {
+  userController.createBooking(key, req.body);
+  //     } else {
+  //         console.log("no session found");
+  //     }
 });
 
 // Delete Booking after scheduled time
-router.delete('/booking/delete/:id', function (req, res) {
-    var id = req.params.id;
-    db.get().createCollection('menu', function (err, col) {
-        col.deleteOne({ _id: new mongodb.ObjectID(id) });
-    });
-    res.json({ success: id })
+router.delete("/booking/delete/:id", function(req, res) {
+  var id = req.params.id;
+  db.get().createCollection("menu", function(err, col) {
+    col.deleteOne({ _id: new mongodb.ObjectID(id) });
+  });
+  res.json({ success: id });
 });
 
 // Create User
 //router.route("/user/create").post(userController.createUser);
 
-router.post(
-    "/user/create", (req, res) => {
+router.post("/user/create", (req, res) => {
+  req.body.sanitized = {
+    firstName: req.sanitize(req.body.firstName),
+    lastName: req.sanitize(req.body.lastName),
+    email: req.sanitize(req.body.email),
+    password: req.sanitize(req.body.password),
+    confirmedPassword: req.sanitize(req.body.confirmedPassword)
+  };
 
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmedPassword
+  } = req.body.sanitized;
 
-        req.body.sanitized = {
-            firstName: req.sanitize(req.body.firstName),
-            lastName: req.sanitize(req.body.lastName),
-            email: req.sanitize(req.body.email),
-            password: req.sanitize(req.body.password),
-            confirmedPassword: req.sanitize(req.body.confirmedPassword)
-        };
+  const newUser = {
+    firstName,
+    lastName,
+    email,
+    password
+  };
 
-        const {
-            firstName,
-            lastName,
-            email,
-            password,
-            confirmedPassword
-        } = req.body.sanitized;
-
-        const newUser = {
-            firstName,
-            lastName,
-            email,
-            password
-        };
-
-
-        userController.createUser(newUser, (err, user) => {
-            if (err) throw err;
-            console.log(user);
-            //res.json({ user });
-            res.redirect("/");
-        });
-        //console.log(req.body);
-        //req.session.user = user;  
-    });
-
+  userController.createUser(newUser, (err, user) => {
+    if (err) throw err;
+    console.log(user);
+    //res.json({ user });
+    res.redirect("/");
+  });
+  //console.log(req.body);
+  //req.session.user = user;
+});
 
 // User Login
 router.route("/user/login/:id").get(userController.userLogin);
@@ -79,4 +74,4 @@ router.route("/user/login/:id").get(userController.userLogin);
 
 module.exports = router;
 
-// DONT FORGET INDEX[0] for booking 
+// DONT FORGET INDEX[0] for booking
