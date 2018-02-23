ae5676b57b0bae54caf0c0c4a267a897fc91f8f4
Pasever <pas1034ever@gmail.com>
2018-02-23 08:26:10 -0500

    sign up

--- models/user.js
+++ models/user.js
@@ -6,18 +6,23 @@ const UserSchema = new Schema({
   firstName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
+    validate:[ fn => fn && fn.length > 2, "Must be at least 2 characters long"]
   },
   lastName: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
+    validate: [ln => ln && ln.lenght > 2, "Must be at least 2 characters long"]
   },
   email: {
     type: String,
     trim: true,
     unique: true,
-    required: true
+    required: true,
+    validate: [function(email) {
+      return /^[a-zA-Z0-9.!#$%&’*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+    }, "'{VALUE}' is not a valid email"],
   },
   password: {
     type: String,
