8a93646de14302c6937dae1b4dc202113260aa5e
Nate Morgan <natmor88@gmail.com>
2018-02-23 22:14:52 -0500

    changed express.static

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(expressSanitizer());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 // app.use("/", index);
 //app.use('/users', users);
