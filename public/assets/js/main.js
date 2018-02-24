4357b4ca04bc7a6f5e7afea15f8a979a91c087c0
Trevor Meadows <tlm04070@gmail.com>
2018-02-23 23:06:46 -0500

    finished project 3

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,14 +1,13 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
   //Start of Loading Overlay
-  $(document).ready(function () {
-    setTimeout(function () {
+  $(document).ready(function() {
+    setTimeout(function() {
       $("#loadingOverlay").fadeOut();
     }, 3000);
   });
   // ./ of Loading Overlay
 
-  // Start of Testimonials Carousel  
+  // Start of Testimonials Carousel
   $("#carousel").carouFredSel({
     circular: true,
     infinite: false,
@@ -21,7 +20,7 @@ $(document).ready(function () {
     },
     pagination: {
       container: "#carousel_indicator",
-      anchorBuilder: function (nr) {
+      anchorBuilder: function(nr) {
         return '<a href="#' + nr + '"></a>';
       }
     }
@@ -33,22 +32,29 @@ $(document).ready(function () {
     offset: {
       top: 100
     }
-  })
+  });
   // ./Offset for Site Navigation
 
   // Start of Return to Top Function
-  $(window).scroll(function () {
-    if ($(this).scrollTop() >= 50) {  
-      $("#return_to_top").fadeIn(100);  
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {
+      $("#return_to_top").fadeIn(100);
     } else {
-      $("#return_to_top").fadeOut(100);  
+      $("#return_to_top").fadeOut(100);
     }
   });
-  $("#return_to_top").click(function () {     
-    $("body,html").animate({
-      scrollTop: 0                    
-    }, 100);
+  $("#return_to_top").click(function() {
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      100
+    );
   });
   // ./Return to Top Function
 
+  //return customer to home page
+  $("#confirmation_btn").on("click", function() {
+    window.location.assign("http://localhost:3000/");
+  });
 });
